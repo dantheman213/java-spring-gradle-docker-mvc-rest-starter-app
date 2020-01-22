c194bbd968c2300ed9f9110d603dc7aa8ac026a9
Dan Gillespie <daniel.gillespie@pluto.tv>
2020-01-21 20:19:31 -0800

    removed unneeded line

--- src/main/java/myapp/controllers/HomeController.java
+++ src/main/java/myapp/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package myapp.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
